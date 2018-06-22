c92e58e958675cf06efe380a1f58660fe4c2d122
JoshuaDanner <jdanner3@gmail.com>
2018-06-22 11:17:21 -0600

    first commit

---
+++ RandomizedPairsWeek6Test/Controllers/ModelControllers/NameController.swift
@@ -0,0 +1,9 @@
+//
+//  NameController.swift
+//  RandomizedPairsWeek6Test
+//
+//  Created by Joshua Danner on 6/22/18.
+//  Copyright © 2018 JoshuaDanner. All rights reserved.
+//
+
+import Foundation
---
+++ RandomizedPairsWeek6Test/Model/Name.swift
@@ -0,0 +1,9 @@
+//
+//  Name.swift
+//  RandomizedPairsWeek6Test
+//
+//  Created by Joshua Danner on 6/22/18.
+//  Copyright © 2018 JoshuaDanner. All rights reserved.
+//
+
+import Foundation
